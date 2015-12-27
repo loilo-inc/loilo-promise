4a48912eaa20cc0baaa4f882b036151bbf50e4db
hayama-junpei <hayama@loilo.tv>
2015-12-27 18:02:40 +0900

    Copyright 一括設定

--- promise-samples-progress/src/main/java/tv/loilo/promise/samples/progress/SampleProgressSpinnerDialogFragment.java
+++ promise-samples-progress/src/main/java/tv/loilo/promise/samples/progress/SampleProgressSpinnerDialogFragment.java
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2015 LoiLo inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package tv.loilo.promise.samples.progress;
 
 import android.app.Dialog;
@@ -30,6 +46,7 @@ import tv.loilo.promise.support.PromiseLoaderCallbacks;
 public class SampleProgressSpinnerDialogFragment extends AppCompatDialogFragment implements PromiseLoaderCallbacks<Void> {
     private static final int LOADER_ID = 0;
 
+    @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
         final ProgressDialog progressDialog = new ProgressDialog(getContext(), getTheme());
